23a633062d4ebf703910197475eeb93289d9405b
Kevinfd12 <kevinfd12@gmail.com>
2023-03-18 14:27:07 -0400

    Added Edit Functionality

--- src/components/searchBar.tsx/inputBar.tsx
+++ src/components/searchBar.tsx/inputBar.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Button } from '../button/button';
 import { ListItem } from '../utils/listItem';
+import { TodoModal } from '../utils/TodoModal';
 import './inputBar.scss';
 
-interface Todo {
+export interface Todo {
   id: number;
   value: string;
 }
@@ -12,7 +13,7 @@ export function InputBar() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [inputValue, setInputValue] = useState('');
   const [editMode, setEditMode] = useState(false);
-  const [newItem, setNewItem] = useState('');
+  const [editedTodo, setEditedTodo] = useState<Todo | undefined>(undefined);
 
   const getNextId = () => {
     let id = 1;
@@ -22,8 +23,7 @@ export function InputBar() {
     return id;
   };
 
-  const handleSubmit = (event: { preventDefault: () => void }) => {
-    event.preventDefault();
+  const handleSubmit = () => {
     if (inputValue !== '') {
       const newTodo = { id: getNextId(), value: inputValue };
       setTodos([...todos, newTodo]);
@@ -31,60 +31,65 @@ export function InputBar() {
     }
   };
 
+  const handleModalSubmit = () => {
+    if (!editedTodo) {
+      return;
+    }
+    const newTodos = todos.filter((todo) => todo.id !== editedTodo.id);
+    setTodos([...newTodos, editedTodo]);
+    handleEditMode(undefined);
+  };
+
+  const handleEditMode = (todo: Todo | undefined) => {
+    setEditMode((isEditOn) => !isEditOn);
+    setEditedTodo(todo);
+  };
+
   const handleDelete = (deleteTodo: Todo) => {
     const todoDelete = todos.filter((todo) => todo.id !== deleteTodo.id);
     setTodos(todoDelete);
-  };
-
-  const handleEdit = () => {
-    setEditMode(true);
   };
 
   return (
     <>
       <div className="inputBar">
         <div className="inputBar__wrapper">
-          <form onSubmit={handleSubmit}>
-            <div className="inputBar__input-wrapper">
-              <label htmlFor="inputBar">Add a task to your to-do list, </label>
-              <div className="inputSubmit">
-                <input
-                  type="text"
-                  className="inputBar__input"
-                  placeholder="add a todo"
-                  value={inputValue}
-                  onChange={(event) => setInputValue(event.target.value)}
-                ></input>
-                <input type="submit" value="submit"></input>
-              </div>
+          <div className="inputBar__input-wrapper">
+            <label htmlFor="inputBar">Add a task to your to-do list, </label>
+            <div className="inputSubmit">
+              <input
+                type="text"
+                className="inputBar__input"
+                placeholder="add a todo"
+                value={inputValue}
+                onChange={(event) => setInputValue(event.target.value)}
+              ></input>
+              <Button value="submit" onButtonClick={handleSubmit}></Button>
             </div>
-          </form>
+          </div>
         </div>
       </div>
 
       <ul>
-        {todos.map((todo) => (
-          <div className="ToDo-Wrapper" key={todo.id}>
-            <p>To-Do Task #{todo.id}</p>
-            <ListItem indexes={todo.id} todos={todo.value}>
-              <Button value={'delete'} onButtonClick={() => handleDelete(todo)}></Button>
-              {editMode ? (
-                <form onSubmit={handleSubmit}>
-                  <input
-                    className="input"
-                    value={newItem}
-                    onChange={(event) => setNewItem(event.target.value)}
-                    placeholder="edit"
-                  ></input>
-                  <input type="submit" value="submit"></input>
-                </form>
-              ) : (
-                <Button value={'edit'} onButtonClick={() => setEditMode(true)}></Button>
-              )}
-            </ListItem>
-          </div>
+        {todos.map((todo, index, arr) => (
+          <ListItem
+            todo={todo}
+            handleDelete={handleDelete}
+            index={index + 1}
+            arrLength={arr.length}
+            handleEditMode={handleEditMode}
+            key={todo.id}
+          />
         ))}
       </ul>
+      {editMode && (
+        <TodoModal
+          handleEditMode={handleEditMode}
+          editedTodo={editedTodo}
+          setEditedTodo={setEditedTodo}
+          handleModalSubmit={handleModalSubmit}
+        />
+      )}
     </>
   );
 }

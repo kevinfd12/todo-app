c18a0c8f22ce03e7ce31a3fa00d2e5fcd5919265
Kevinfd12 <kevinfd12@gmail.com>
2023-03-31 21:44:03 -0400

    improving-styles

--- src/components/todoHeader/todoHeader.tsx
+++ src/components/todoHeader/todoHeader.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import img1 from '../../assets/ForeGround-removebg-preview.png';
 import img2 from '../../assets/Background.jpg';
+import img3 from '../../assets/CounterTop-removebg-preview.png';
 import './todoHeader.scss';
 
 export const TodoHeader = () => {
   return (
     <div className="todoHeader__wrapper">
-      <img src={img1} className="todoHeader__Foreground" alt="Groceries"></img>
-      <img src={img2} className="todoHeader__Midground" alt="Groceries"></img>
+      <img src={img1} className="todoHeader__Foreground" alt="Groceries1"></img>
+      <img src={img2} className="todoHeader__Midground" alt="Groceries2"></img>
+      <img src={img3} className="todoHeader__CounterTop" alt="Groceries3"></img>
     </div>
   );
 };

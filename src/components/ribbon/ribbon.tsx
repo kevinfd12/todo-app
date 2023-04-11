aefbd8cacbd953f1dca24caf13ea847816985e16
Kevinfd12 <kevinfd12@gmail.com>
2023-04-10 23:20:18 -0400

    added fading

--- src/components/ribbon/ribbon.tsx
+++ src/components/ribbon/ribbon.tsx
@@ -1,7 +1,19 @@
 import './ribbon.scss';
+import { useEffect, useState } from 'react';
 
-export const Ribbon = () => (
-  <div className={`RibbonBox--rt`}>
-    <span className={`Ribbon--rt red`} />
-  </div>
-);
+export const Ribbon = () => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 5000); //this is a delay of 5 seconds before it even starts to fade.
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className={`RibbonBox--rt`}>
+      <span className={`Ribbon--rt red ${visible ? 'visible' : ''}`} />
+    </div>
+  );
+};

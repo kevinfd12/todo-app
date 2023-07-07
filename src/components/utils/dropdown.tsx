bf014ed72b9d9c288951fb4aceca2b0e3aef2f6a
Kevinfd12 <kevinfd12@gmail.com>
2023-07-07 12:57:18 -0400

    more edits

--- src/components/utils/dropdown.tsx
+++ src/components/utils/dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './dropdown.scss';
 import { Todo, currentColumn } from '../main/Main';
 
@@ -8,11 +8,25 @@ interface DropdownProps {
   setTodos: (todo: (todo: Todo[]) => Todo[]) => void;
 }
 
-export function Dropdown({ color = '#D6CFC7', currentTodo, setTodos }: DropdownProps) {
+export function Dropdown(this: any, { color = '#D6CFC7', currentTodo, setTodos }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [isBottom, setIsBottom] = useState(false);
+  const container = useRef<HTMLDivElement>(null);
 
-  const dropdownHandler = () => {
+  useEffect(() => {
+    window.addEventListener('mousedown', (e: any) => handleClickOutside(e));
+
+    return () => {
+      window.removeEventListener('mousedown', (e: any) => handleClickOutside(e));
+    };
+  }, [isOpen]);
+
+  const dropdownHandler = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const { bottom } = e.currentTarget.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    setIsBottom(windowHeight - bottom < 110);
     setIsOpen((isOpen) => !isOpen);
+    console.log(isOpen);
   };
 
   const onOptionClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -24,14 +38,25 @@ export function Dropdown({ color = '#D6CFC7', currentTodo, setTodos }: DropdownP
     });
   };
 
+  const handleClickOutside = (e: any) => {
+    if (container.current && !container.current.contains(e.target)) {
+      setIsOpen(!isOpen);
+    }
+  };
+
   return (
     <>
-      <div className="dropdown" onClick={dropdownHandler} style={{ backgroundColor: color }}>
+      <div
+        className="dropdown"
+        onClick={dropdownHandler}
+        style={{ backgroundColor: color }}
+        ref={container}
+      >
         <p className="arrow">{currentTodo.position}</p>
         <div className={`arrow__${isOpen ? `up` : `down`}`}>{'<'}</div>
 
         {isOpen && (
-          <div className="dropdown__list">
+          <div className={`dropdown__list${isBottom ? `__bottom` : `__normal`}`}>
             <div className="dropdown__option" onClick={onOptionClick}>
               {currentColumn.isOpen}
             </div>
